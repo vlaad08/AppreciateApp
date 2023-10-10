e71ee8a7b341c562c997fec69f24a546f8994945
vlaad08 <62983956+vlaad08@users.noreply.github.com>
2023-10-10 19:09:56 +0200

    Notification
    setting up permission and scheduled notification

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@ import AppreciationScreen from './screens/AppreciationsScreen';
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import SettingScreen from './screens/SettingsScreen';
-
-
+import * as Notifications from 'expo-notifications'
+import * as Device from 'expo-device'
+import { Platform } from 'react-native';
 
 type AppNavigatorParamList = {
   Home: undefined;
@@ -41,4 +42,54 @@ const App: React.FC = () => {
   );
 };
 
+
+
 export default App;
+
+
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: false,
+//     shouldSetBadge: false,
+//   }),
+// });
+
+
+
+
+
+
+
+async function registerForNotificationsAsync() {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    else
+    {
+      // const dailyNotification = async () => {
+      //   const now = new Date();
+      //   const tomorrow = new Date(now);
+      //   tomorrow.setDate(tomorrow.getDate() + 1);
+      //   tomorrow.setHours(20, 0, 0, 0); 
+      //   await Notifications.scheduleNotificationAsync({
+      //     content: {
+      //       title: 'What are you grateful for today?',
+      //       body: 'Come in and log what made you grateful for today!',
+      //     },
+      //     trigger: {
+      //       seconds: 5
+      //     }
+      //   });
+
+      // };
+      // dailyNotification();
+    }
+  } 
+}
+
+registerForNotificationsAsync();
